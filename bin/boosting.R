3cbac83d7a98619fd2a2852666e4f32e707e4552
Alex Kroeger <alexrkroeger@gmail.com>
2016-10-18 23:20:06 -0400

    Altered boosting.R to vary the parameters and find the model that minimized CV error.

--- bin/boosting.R
+++ bin/boosting.R
@@ -1,4 +1,3 @@
-
 ## Clear
 rm(list=ls())
 
@@ -8,9 +7,11 @@ library(randomForest)
 library(data.table)
 library(gbm)
 
+
 ## Read in training dataset
 train <- fread("../data/train.csv")
 names(train)
+submission_file <- "../sample_submission.csv"
 
 ## Convert character to freakin' factor variables
 # list of character vars
@@ -20,8 +21,40 @@ cont_var <- names(train)[which(sapply(train,is.numeric))]
 
 rm(cat_var,cont_var)
 
+## Uncomment for random sample
+#sample <- sample(1:nrow(train),500)
+#train <- train[sample,]
+
 ## Run boosting model
-boost.model <- gbm(loss~.-id,data=train,distribution="gaussian",n.trees=5000,interaction.depth = 1)
+## CV error from dif. models
+gbmcv <- function(list) {
+  shrink=list[[1]]
+  depth=list[[2]]
+  gbm.model <- gbm(loss~.-id,data=train,shrinkage=shrink,distribution="gaussian",n.trees=5000,cv.folds=10,interaction.depth=depth)
+  c(shrink,depth,min(gbm.model$cv.error),which.min(gbm.model$cv.error))
+} 
+
+## Vector of shrinkage params
+num_depths <- 4
+max_shrink <- 0.02
+shrink.params <- sort(rep(seq(0.001,max_shrink,0.001),times=num_depths))
+depths <- rep(seq(1,num_depths,1),length(shrink.params)/num_depths)
+
+params <- cbind(shrink.params,depths)
+list <- split(params,seq(nrow(params)))
+
+#result = apply(X=params,MARGIN=1,FUN=gbmcv)
+result = sapply(list,gbmcv)
+result = t(result)
+
+best.shrink = result[which.min(result[,3]),1]
+best.depth = result[which.min(result[,3]),2]
+
+gbm.model <- gbm(loss~.-id,data=train,shrinkage=best.shrink,
+                 distribution="gaussian",n.trees=10000,
+                 cv.folds=10,interaction.depth=best.depth)
+
+best.ntree <- which.min(gbm.model$cv.error)
 
 ## Predict test set with boosting
 test <- fread("../data/test.csv")
@@ -29,13 +62,12 @@ test <- fread("../data/test.csv")
 # Again, convert character to factor
 cat_var <- names(test)[which(sapply(test, is.character))]
 test[,(cat_var):=lapply(.SD, as.factor),.SDcols=cat_var]
-yhat <- predict(boost.model,newdata=test,n.trees=5000)
+yhat <- predict(gbm.model,newdata=test,n.trees=best.ntree)
 
 rm(cat_var)
 
-outboost <- as.data.frame(cbind(test$id,yhat))
+submission_file <- "../sample_submission.csv"
+submission <- fread(submission_file,colClasses=c("integer","numeric"))
+submission$loss <- yhat
 
-outnames <- c("id","loss")
-names(outboost) <- outnames
-
-write.table(outboost,"../output/outboost.csv",sep=",",row.names=FALSE)
+write.csv(submission,"../output/outboost_optimized.csv",row.names=FALSE)
