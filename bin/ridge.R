72acf08d1ba5fae363a8f63bd5f650dd49e98760
Alex Kroeger <alexrkroeger@gmail.com>
2016-10-30 01:12:32 -0400

    Addressed some nonlinearity issues in ridge.R.

--- bin/ridge.R
+++ bin/ridge.R
@@ -10,8 +10,8 @@ train <- fread("../data/train.csv")
 test <- fread("../data/test.csv")
 
 ## Uncomment for random sample
-sample <- sample(1:nrow(train),50000)
-train <- train[sample,]
+#sample <- sample(1:nrow(train),70000)
+#train <- train[sample,]
 
 test$loss <- 0
 test$test <- 1
@@ -22,6 +22,23 @@ testset <- which(data$test == 1)
 trainset <- which(data$test == 0)
 
 rm(test,train)
+
+## Drop cont11 due to collinearity
+data <- subset(data,select=-c(cont11))
+
+## Put certain vars in bins to address nonlinearity
+data$cont1.1 <- (as.numeric(data$cont1 < 0.6))*data$cont1
+data$cont1.2 <- (as.numeric(data$cont1 >= 0.6))*data$cont1
+
+data$cont13.1 <- as.numeric(data$cont13 < 0.5)
+data$cont13.2 <- as.numeric(data$cont13 >= 0.5)
+
+data$cont14.1 <- (as.numeric(data$cont14 < 0.25))*data$cont14
+data$cont14.2 <- (as.numeric(data$cont14 >= 0.25 & data$cont14 < 0.3))*data$cont14
+data$cont14.3 <- (as.numeric(data$cont14 >= 0.3))*data$cont14
+
+# Drop original variables
+data <- subset(data,select=-c(cont1,cont3,cont14))
 
 ## Matrix for regression
 X <- model.matrix(loss~.-id-test,data)
