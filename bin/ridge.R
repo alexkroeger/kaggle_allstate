bd1caf3953d4f43edc3b0fa004535402e6f18c76
Alex Kroeger <alexrkroeger@gmail.com>
2016-10-30 13:01:45 -0400

    Fixed the treatment of nonlinear vars in ridge.R.

--- bin/ridge.R
+++ bin/ridge.R
@@ -27,18 +27,13 @@ rm(test,train)
 data <- subset(data,select=-c(cont11))
 
 ## Put certain vars in bins to address nonlinearity
-data$cont1.1 <- (as.numeric(data$cont1 < 0.6))*data$cont1
-data$cont1.2 <- (as.numeric(data$cont1 >= 0.6))*data$cont1
+data$cont1.1 <- ((as.numeric(data$cont1 > 0.6))*data$cont1)-0.6
 
-data$cont13.1 <- as.numeric(data$cont13 < 0.5)
-data$cont13.2 <- as.numeric(data$cont13 >= 0.5)
+data$cont13.1 <- ((as.numeric(data$cont13 > 0.5))*data$cont13)-0.5
 
-data$cont14.1 <- (as.numeric(data$cont14 < 0.25))*data$cont14
-data$cont14.2 <- (as.numeric(data$cont14 >= 0.25 & data$cont14 < 0.3))*data$cont14
-data$cont14.3 <- (as.numeric(data$cont14 >= 0.3))*data$cont14
+data$cont14.1 <- ((as.numeric(data$cont14 > 0.25))*data$cont14)-0.25
+data$cont14.2 <- ((as.numeric(data$cont14 > 0.3))*data$cont14) - 0.3
 
-# Drop original variables
-data <- subset(data,select=-c(cont1,cont3,cont14))
 
 ## Matrix for regression
 X <- model.matrix(loss~.-id-test,data)
