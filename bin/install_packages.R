eb95d045b329d6bc4083c5fa7915390eef50408a
Alex Kroeger <alexrkroeger@gmail.com>
2016-11-17 01:46:50 -0500

    Altered install_packages.R to specify a path.

--- bin/install_packages.R
+++ bin/install_packages.R
@@ -1,4 +1,4 @@
-install.packages('devtools')
-install.packages('data.table')
-install.packages('xgboost')
-install_github('nathanvan/parallelsugar')
+install.packages('devtools',lib="/tmp/Rtmp25Gc5B/downloaded_packages")
+install.packages('data.table',lib="/tmp/Rtmp25Gc5B/downloaded_packages")
+install.packages('xgboost',lib="/tmp/Rtmp25Gc5B/downloaded_packages")
+install_github('nathanvan/parallelsugar',lib="/tmp/Rtmp25Gc5B/downloaded_packages")
